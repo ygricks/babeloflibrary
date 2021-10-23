ad1bbcff3ffe20164dfada11d96eea2d9f3ee0a8
Ygrick <cujbaigor90@gmail.com>
2021-10-23 13:54:36 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 
@@ -6,6 +7,17 @@ const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('parrot_test')
+    .setDescription('simple parrot test')
+    .setVersion('0.0.1')
+    .addTag('authors')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  console.log('listen port:', port);
   await app.listen(port);
 }
 bootstrap();
