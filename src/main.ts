323a79839eb587f38f87be74d377e4ea8b86c600
Ygrick <cujbaigor90@gmail.com>
2021-10-26 19:16:41 +0300

    swagger use detail from package.json

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+const detail = require('../package.json');
 
 const port = process.env.PORT;
 
@@ -9,10 +10,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('babeloflibrary')
-    .setDescription('babel of library')
-    .setVersion('0.0.1')
-    .addTag('authors')
+    .setTitle(detail.name)
+    .setDescription(detail.description)
+    .setVersion(detail.version)
+    .addTag(detail.tag)
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
