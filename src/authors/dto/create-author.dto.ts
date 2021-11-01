7bc182044bb17e2c24949975d54226bdeb9d5f38
Ygrick <cujbaigor90@gmail.com>
2021-11-02 00:43:32 +0200

    swagger describe entities create dto

--- src/authors/dto/create-author.dto.ts
+++ src/authors/dto/create-author.dto.ts
@@ -1,17 +1,35 @@
 import { IsNotEmpty, Length, IsDate } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateAuthorDto {
   @IsNotEmpty()
   @Length(3, 100)
+  @ApiProperty({
+    description: 'Author first name',
+    minLength: 3,
+    maxLength: 100,
+    type: 'string',
+  })
   first_name: string;
 
   @IsNotEmpty()
   @Length(3, 100)
+  @ApiProperty({
+    description: 'Author last name',
+    minLength: 3,
+    maxLength: 100,
+    type: 'string',
+  })
   last_name: string;
 
   @IsNotEmpty()
   @Type(() => Date)
   @IsDate()
+  @ApiProperty({
+    description: 'Author birthday, iso-8601',
+    format: 'YYYY-MM-DD',
+    type: 'Date',
+  })
   birthday: Date;
 }
--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,20 +1,42 @@
 import { IsNotEmpty, Length, IsDate } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBookDto {
   @IsNotEmpty()
   @Length(3, 100)
+  @ApiProperty({
+    description: 'Book title',
+    minLength: 3,
+    maxLength: 100,
+    type: 'string',
+  })
   title: string;
 
   @IsNotEmpty()
   @Length(6, 100)
+  @ApiProperty({
+    description: 'Book unique IBAN, isbn?',
+    minLength: 3,
+    maxLength: 100,
+    type: 'string',
+  })
   iban: string;
 
   @IsNotEmpty()
   @Type(() => Date)
   @IsDate()
+  @ApiProperty({
+    description: 'Book publication date, iso-8601',
+    format: 'YYYY-MM-DD',
+    type: 'Date',
+  })
   published_at: Date;
 
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'Book author ID',
+    type: 'ObjectID.toString()',
+  })
   author: string;
 }
