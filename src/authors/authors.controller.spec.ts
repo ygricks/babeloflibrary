bdd581506bfd516e210be66ca08aa7bff544a3e8
Ygrick <cujbaigor90@gmail.com>
2021-11-03 02:34:14 +0200

    add author & book controller tests

--- src/authors/authors.controller.spec.ts
+++ src/authors/authors.controller.spec.ts
@@ -1,3 +1,4 @@
+import { Controller } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthorsController } from './authors.controller';
 import { AuthorsService } from './authors.service';
@@ -5,16 +6,82 @@ import { AuthorsService } from './authors.service';
 describe('AuthorsController', () => {
   let controller: AuthorsController;
 
+  const mockAuthor = {
+    create: jest.fn((dto) => {
+      return {
+        _id: '' + Date.now(),
+        ...dto,
+      };
+    }),
+    update: jest.fn((id, dto) => {
+      return {
+        _id: id,
+        ...dto,
+      };
+    }),
+    findAll: jest.fn(() => {
+      return [];
+    }),
+    findBooks: jest.fn((_id) => {
+      return [];
+    }),
+    findOne: jest.fn((_id) => {
+      return new Object();
+    }),
+    remove: jest.fn((_id) => {
+      return Math.floor(Math.random() * 10) % 2 == 0;
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthorsController],
       providers: [AuthorsService],
-    }).compile();
+    })
+      .overrideProvider(AuthorsService)
+      .useValue(mockAuthor)
+      .compile();
 
     controller = module.get<AuthorsController>(AuthorsController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  const dto = {
+    first_name: 'Ion',
+    last_name: 'creanga',
+    birthday: new Date('1837-04-01'),
+  };
+
+  it('should create a author', () => {
+    expect(controller.create(dto)).toEqual({
+      _id: expect.any(String),
+      ...dto,
+    });
+  });
+
+  it('should updape a author', () => {
+    expect(controller.update('12', dto)).toEqual({
+      _id: '12',
+      ...dto,
+    });
+  });
+
+  it('should findAll authors', () => {
+    expect(controller.findAll()).toEqual(expect.any(Array));
+  });
+
+  it('should findBooks of author', () => {
+    expect(controller.findBooks('11')).toEqual(expect.any(Array));
+  });
+
+  it('should findOne author', () => {
+    expect(controller.findOne('11')).toEqual(expect.any(Object));
+  });
+
+  it('should remove author', () => {
+    expect(controller.remove('11')).toEqual(expect.any(Boolean));
   });
 });
--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -5,16 +5,76 @@ import { BooksService } from './books.service';
 describe('BooksController', () => {
   let controller: BooksController;
 
+  const mockBook = {
+    create: jest.fn((dto) => {
+      return {
+        _id: '' + Date.now(),
+        ...dto,
+      };
+    }),
+    findOne: jest.fn((_id) => {
+      return new Object();
+    }),
+    update: jest.fn((id, dto) => {
+      return {
+        _id: id,
+        ...dto,
+      };
+    }),
+    findAll: jest.fn(() => {
+      return [];
+    }),
+    remove: jest.fn((_id) => {
+      return Math.floor(Math.random() * 10) % 2 == 0;
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BooksController],
       providers: [BooksService],
-    }).compile();
+    })
+      .overrideProvider(BooksService)
+      .useValue(mockBook)
+      .compile();
 
     controller = module.get<BooksController>(BooksController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  const dto = {
+    title: 'the war and peace',
+    iban: '023847520',
+    published_at: new Date('1777-07-07'),
+    author: '200',
+  };
+
+  it('should create a book', () => {
+    expect(controller.create(dto)).toEqual({
+      _id: expect.any(String),
+      ...dto,
+    });
+  });
+
+  it('should findOne book', () => {
+    expect(controller.findOne('11')).toEqual(expect.any(Object));
+  });
+
+  it('should updape a book', () => {
+    expect(controller.update('23', dto)).toEqual({
+      _id: '23',
+      ...dto,
+    });
+  });
+
+  it('should findAll books', () => {
+    expect(controller.findAll()).toEqual(expect.any(Array));
+  });
+
+  it('should remove a book', () => {
+    expect(controller.remove('11')).toEqual(expect.any(Boolean));
   });
 });
