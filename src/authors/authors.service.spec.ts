cf07ce6d1ef4e344b9e5c572fa3b9ac34a8b935a
Ygrick <cujbaigor90@gmail.com>
2021-11-04 07:25:41 +0200

    add test to service books/authors

--- src/authors/authors.service.spec.ts
+++ src/authors/authors.service.spec.ts
@@ -5,19 +5,38 @@ import { Author } from './entities/author.entity';
 import { Book } from '../books/entities/book.entity';
 import { ObjectID } from 'mongodb';
 
+const dto = {
+  first_name: 'Ion',
+  last_name: 'Creanga',
+  birthday: new Date('1837-04-01'),
+};
+
 describe('AuthorsService', () => {
   let service: AuthorsService;
 
   const mockAuthorProvider = {
-    create: jest.fn().mockImplementation((dto) => dto),
     save: jest.fn().mockImplementation((author) =>
       Promise.resolve({
         _id: ObjectID('6181c1055738e7daf06d05ce'),
         ...author,
       }),
     ),
+    find: jest.fn().mockImplementation(() => {
+      return Promise.resolve([]);
+    }),
+    findOne: jest.fn().mockImplementation((__id) => {
+      return Promise.resolve(Object.assign(new Author(), dto));
+    }),
+    delete: jest.fn().mockImplementation((_id) => {
+      const message = new Object({ raw: { result: { rm: 1, do: 1 } } });
+      return Promise.resolve(message);
+    }),
   };
-  const mockBookProvider = {};
+  const mockBookProvider = {
+    find: jest.fn().mockImplementation(() => {
+      return Promise.resolve([]);
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -37,20 +56,34 @@ describe('AuthorsService', () => {
     service = module.get<AuthorsService>(AuthorsService);
   });
 
-  // it('should be defined', () => {
-  //   expect(service).toBeDefined();
-  // });
-
-  const dto = {
-    first_name: 'Ion',
-    last_name: 'creanga',
-    birthday: new Date('1837-04-01'),
-  };
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
 
   it('should create and return Author record', async () => {
     expect(await service.create(dto)).toEqual({
-      _id: expect.any('ObjectID'),
+      _id: expect.any(ObjectID),
       ...dto,
     });
+  });
+
+  it('should return all Authors', async () => {
+    const a = await service.findAll();
+    expect(await service.findAll()).toEqual(expect.any(Array));
+  });
+
+  it('should return one Author', async () => {
+    const _id = ObjectID('6181c1055738e7daf06d05ce');
+    expect(await service.findOne(_id)).toEqual(dto);
+  });
+
+  it('should return Author Books', async () => {
+    const _id = ObjectID('6181c1055738e7daf06d05ce');
+    expect(await service.findBooks(_id)).toEqual(expect.any(Array));
+  });
+
+  it('should remove Author', async () => {
+    const _id = ObjectID('6181c1055738e7daf06d05ce');
+    expect(await service.remove(_id)).toEqual(expect.any(Object));
   });
 });
--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,12 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { BooksService } from './books.service';
+import { Author } from '../authors/entities/author.entity';
+import { Book } from './entities/book.entity';
+import { ObjectID } from 'mongodb';
+
+const dto = {
+  title: 'abecedar',
+  iban: 'a7oe80u7a0o',
+  published_at: new Date('2000-03-03'),
+  author: '6181c1055738e7daf06d05ce',
+};
 
 describe('BooksService', () => {
   let service: BooksService;
+  const mockBookProvider = {
+    save: jest.fn().mockImplementation((book) =>
+      Promise.resolve({
+        _id: ObjectID('6181c1055738e7daf06d05ce'),
+        ...book,
+      }),
+    ),
+    find: jest.fn().mockImplementation(() => {
+      return Promise.resolve([]);
+    }),
+    findOne: jest.fn().mockImplementation((...params) => {
+      return Promise.resolve(Object.assign(new Book(), dto));
+    }),
+    delete: jest.fn().mockImplementation((__id) => {
+      const message = new Object({ raw: { result: { rm: 1, do: 1 } } });
+      return Promise.resolve(message);
+    }),
+  };
+  const mockAuthorProvider = {
+    find: jest.fn().mockImplementation(() => {
+      return Promise.resolve([]);
+    }),
+
+    findOne: jest.fn().mockImplementation((...params) => {
+      return Promise.resolve(
+        Object.assign(new Author(), { first_name: 'Ion' }),
+      );
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BooksService],
+      providers: [
+        BooksService,
+        {
+          provide: getRepositoryToken(Book),
+          useValue: mockBookProvider,
+        },
+        {
+          provide: getRepositoryToken(Author),
+          useValue: mockAuthorProvider,
+        },
+      ],
     }).compile();
 
     service = module.get<BooksService>(BooksService);
@@ -14,5 +64,28 @@ describe('BooksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // can't situ
+  // it('should create and return Book record', async () => {
+  //   expect(await service.create(dto)).toEqual({
+  //     _id: expect.any(ObjectID),
+  //     ...dto,
+  //   });
+  // });
+
+  it('should return all Books', async () => {
+    const a = await service.findAll();
+    expect(await service.findAll()).toEqual(expect.any(Array));
+  });
+
+  it('should return one Book', async () => {
+    const _id = ObjectID('6181c1055738e7daf06d05ce');
+    expect(await service.findOne(_id)).toEqual(dto);
+  });
+
+  it('should remove Book', async () => {
+    const _id = ObjectID('6181c1055738e7daf06d05ce');
+    expect(await service.remove(_id)).toEqual(expect.any(Object));
   });
 });
